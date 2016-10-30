4392868c52ab83bbf7a41b0cb1c36058b2889e67
Gabriel Wolf <bladerunner@posteo.de>
2016-10-30 20:16:28 +0100

    zaehlebuchstaben.py -- Upgrade: Script ermittelt die Häufigkeit aller Buchstaben einer Datei in absteigender Reihenfolge

--- zaehlebuchstaben.py
+++ zaehlebuchstaben.py
@@ -1,7 +1,41 @@
-def countLetters(string, letter):
-    return string.count(letter)
+import string
 
-string = input('Bitte geben sie einen Text ein: ')
-buchstabe = input('Welcher Buchstabe soll gesucht werden? ')
-count = countLetters(string, buchstabe)
-print('Der Buchstabe',buchstabe,'kommt',count,'x vor.')
+print('Häufigkeit von Buchstaben eines Textes.')
+fname = input('Welche Datei soll verwendet werden? ')
+#fname = 'mbox.txt'
+try:
+    fhand = open(fname,'r+')
+except:
+    print('Datei konnte nicht geöffnet werden:', fname)
+    exit()
+
+file = fhand.read()
+
+file = file.lower()                   # nur Kleinbuchstaben
+file = file.replace(' ', '')          # keine Leerzeichen
+file = file.replace('\n', '')         # keine Zeilensprünge
+file = file.replace('\t', '')         # keine Tabulaturzeichen
+
+i = 0
+while i < 10:                         # keine Zahlen
+    file = file.replace(str(i), '')
+    i = i + 1
+
+punctuation = string.punctuation      # keine Interpunktion
+for letter in punctuation:
+    file = file.replace(letter, '')
+
+dictionary = dict()
+
+for letter in file:
+    dictionary[letter] = dictionary.get(letter,0) + 1
+
+lst = list()
+
+for buchstabe, count in dictionary.items():
+    lst.append( ((count), (buchstabe)) )
+
+lst.sort(reverse=True)
+
+for buchstabe in lst:
+    print(buchstabe[1], buchstabe[0])
