e7470fdbabc16849c3c1a245640ba6ff918e24c9
Gabriel Wolf <gabriel.wolf@wolfzeit.com>
2017-02-06 13:05:24 +0100

    geodata Assignment -- Projekt mit mehreren Python scripts, das die Google Maps API benutzt. Aufgabe war einen eigenen Ort zu where.data hinzuzufügen, geoload.py auszuführen, geodump.py auszuführen und where.html aufzurufen. Dabei musste es mit Screenshots dokumentieren.

---
+++ Python 2/geodata/where.js
@@ -0,0 +1,9 @@
+myData = [
+[52.5125322,13.3269446, '10623 Berlin, Germany'],
+[42.3398067,-71.0891717, '360 Huntington Ave, Boston, MA 02115, USA'],
+[32.7767783,35.0231271, 'Haifa, 3200003, Israel'],
+[18.4638871,73.8679719, 'No.666, Upper Indiranagar, Bibwewadi, Pune, Maharashtra 411037, India'],
+[38.9869183,-76.9425543, 'College Park, MD 20742, USA'],
+[42.4074843,-71.1190232, '419 Boston Ave, Medford, MA 02155‎, USA'],
+[-37.9114731,145.1344641, 'Wellington Road, Clayton VIC 3800, Australia']
+];
